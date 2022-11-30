3d70ca20710299bdfd15750d91278d8a8f9d9e52
angelayke <angebee2605@aol.com>
2022-11-29 21:41:22 -0500

    form contact en cours - pas fini detre complété

--- src/app/formulaire-contact/formulaire-contact.component.ts
+++ src/app/formulaire-contact/formulaire-contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulaire-contact',
@@ -7,11 +8,37 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./formulaire-contact.component.css']
 })
 export class FormulaireContactComponent implements OnInit {
-  name = new FormControl('');
-  
+  contactForm = new FormGroup({
+        prenom : new FormControl('', Validators.required),
+        nom : new FormControl('', Validators.required),
+        email : new FormControl('', Validators.required),
+        tel : new FormControl(''),
+        preference : new FormControl (''),
+    
+        //motif = new FormGroup({
+        //  question = new FormControl('Question au sujet de ce forfait'),
+        //  prix = new FormControl('Demande de prix'),
+        //  remarque = new FormControl('Remarque à propos de ce forfait')
+        //});
+        message : new FormControl(''),
+        //aboutUs = new FormGroup({
+        //  social = new FormControl('Réseaux sociaux'),
+        //  tv = new FormControl('Télévision'),
+        //  journaux = new FormControl('Journeaux'),
+        //  ami = new FormControl('Recommandé par un(e) ami(e), collègue, etc'),
+        //  internet = new FormControl('Recherche internet (Google, Bing, etc)'),
+        //  autre = new FormControl('')
+        //})
+  })
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.contactForm.value);
   }
 
 }
