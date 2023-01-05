c0910bdf26581c582b9556360c26190706f371f8
angelayke <angebee2605@aol.com>
2023-01-04 19:12:48 -0500

    arrangement pour que les categories dans la nav apparaissent - mais quand je suis sur la page routée par forfait hotel et categories ne se deroulent plus ainsique menu burger

--- src/app/tableau-forfaits/tableau-forfaits.component.ts
+++ src/app/tableau-forfaits/tableau-forfaits.component.ts
@@ -3,18 +3,19 @@ import {AfterViewInit, Component, ViewChild, OnInit} from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 import { FORFAITS } from '../mock-forfaits';
+import { Forfait } from '../forfait';
 
 @Component({
   selector: 'app-tableau-forfaits',
   templateUrl: './tableau-forfaits.component.html',
   styleUrls: ['./tableau-forfaits.component.css']
 })
-export class TableauForfaitsComponent implements OnInit {
+export class TableauForfaitsComponent implements AfterViewInit {
   // 'nom', 'description', 'code', 'categories', 'etablissement', 'dateDebut', 'dateFin', 'prix',  'nouveauprix', 'avis', 'premium', 'color'
   //'position', 'name', 'weight', 'symbol'
 
   displayedColumns: string[] = ['code', 'nom', 'categories', 'etablissement', 'dateDebut', 'dateFin', 'prix',  'nouveauprix', 'premium'];
-  dataSource = new MatTableDataSource(FORFAITS);
+  dataSource = new MatTableDataSource<Forfait>(FORFAITS);
   
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -23,8 +24,8 @@ export class TableauForfaitsComponent implements OnInit {
     this.dataSource.paginator = this.paginator;
   }
 
-  ngOnInit(): void {
-  }
+  //ngOnInit(): void {
+ // }
 
 }
 
